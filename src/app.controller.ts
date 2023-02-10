db12ed7fafc6328467f0ad12335965e8c7988ab2
Omogod <godstimeomoregie0@gmail.com>
2023-02-10 12:20:09 +0100

    initialized nest and added qrcode controller

---
+++ src/app.controller.ts
@@ -0,0 +1,35 @@
+import { Controller, Get, Res } from '@nestjs/common';
+import { AppService } from './app.service';
+import * as qrcode from 'qrcode';
+
+@Controller('qr')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('hello')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  // @Get('qrcode')
+  // async getQRCode(@Res() res): Promise<string> {
+  //   try {
+  //     const data = this.appService.getQRCode();
+  //     return res.status(200).json(data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // }
+}
+
+// import { Controller, Get, Res } from '@nestjs/common';
+
+@Controller('qr')
+export class QRController {
+  @Get('qrcode')
+  async generateQR(@Res() res: any) {
+    const link = 'http://www.omdbapi.com/?s=great&apikey=1cb2ace2';
+    const qr = await qrcode.toDataURL(link);
+    res.status(200).json({ qr });
+  }
+}
---
+++ src/app.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Res } from '@nestjs/common';
+import * as qrcode from 'qrcode';
+
+@Injectable()
+export class AppService {
+  getHello(): string {
+    return 'Hello World!';
+  }
+
+  getQRCode(@Res() res) {
+    const link = 'http://www.omdbapi.com/?i=tt3896198&apikey=1cb2ace2';
+    const qr = qrcode.toDataURL(link);
+    res.status(200).json({ qr });
+  }
+}
